115ce155e2139c7febf8fae440d3fb949f160a38
MN10101 <mamocool3@gmail.com>
2024-10-16 21:44:07 +0200

    Email

--- language_learning/settings.py
+++ language_learning/settings.py
@@ -123,7 +123,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'admin@j-education.com' 
+EMAIL_HOST_USER = 'jonathanerasmusdavies43@gmail.com' 
 EMAIL_HOST_PASSWORD = 'ksgm vamj qwap tftm'  
 
 
