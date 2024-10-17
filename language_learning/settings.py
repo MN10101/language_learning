7661678b5b5bd5941bef2245190860269a5859d8
MN10101 <mamocool3@gmail.com>
2024-10-17 15:08:50 +0200

    Email settings

--- language_learning/settings.py
+++ language_learning/settings.py
@@ -119,7 +119,7 @@ LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'welcome'
 
 # Email settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
