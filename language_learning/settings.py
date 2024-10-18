d2bce4573896fc5ca35392109e73c46dbaa49f5f
MN10101 <mamocool3@gmail.com>
2024-10-18 12:36:47 +0200

    Fix

--- language_learning/settings.py
+++ language_learning/settings.py
@@ -68,28 +68,28 @@ WSGI_APPLICATION = 'language_learning.wsgi.application'
 
 # Database configuration
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'language_learning_db',
-#         'USER': 'postgres',
-#         'PASSWORD': 'mamo',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ["POSTGRES_DB"],
-        "USER": os.environ["POSTGRES_USER"],
-        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
-        "HOST": os.environ["POSTGRES_HOST"],
-        "PORT": os.environ["POSTGRES_PORT"],
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'language_learning_db',
+        'USER': 'postgres',
+        'PASSWORD': 'mamo',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": os.environ["POSTGRES_DB"],
+#         "USER": os.environ["POSTGRES_USER"],
+#         "PASSWORD": os.environ["POSTGRES_PASSWORD"],
+#         "HOST": os.environ["POSTGRES_HOST"],
+#         "PORT": os.environ["POSTGRES_PORT"],
+#     }
+# }
 
 
 # Password validation
