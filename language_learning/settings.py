56d2ab134fb6aae1c3f1b73f3218e69ac52b6087
MN10101 <mamocool3@gmail.com>
2024-10-29 15:31:39 +0100

    MIDDLEWARE

--- language_learning/settings.py
+++ language_learning/settings.py
@@ -43,6 +43,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'learning.middleware.DisableCacheMiddleware',
 ]
 
 ROOT_URLCONF = 'language_learning.urls'
