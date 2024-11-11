6b9d4b2a8e8aaa494c7bc890cc43bb2029579295
MN10101 <mamocool3@gmail.com>
2024-11-11 20:23:29 +0100

    Fixed

--- learning/views.py
+++ learning/views.py
@@ -41,7 +41,6 @@ from django.views.decorators.cache import never_cache
 from django.core.mail import send_mail
 from django.conf import settings
 from django.contrib.auth.models import Group
-from django.contrib import CustomUserCreationForm
 
 
 
