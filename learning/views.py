f5f12ba742d1c6911862bef696cd477828d65b37
MN10101 <mamocool3@gmail.com>
2024-11-11 20:01:17 +0100

    Fixed

--- learning/views.py
+++ learning/views.py
@@ -41,7 +41,8 @@ from django.views.decorators.cache import never_cache
 from django.core.mail import send_mail
 from django.conf import settings
 from django.contrib.auth.models import Group
-from .forms import CustomUserCreationForm
+from learning.forms import CustomUserCreationForm
+
 
 
 logger = logging.getLogger(__name__)
