c0426ef99d8fd5cbea4f1f32e3cc703725ca7e8a
MN10101 <mamocool3@gmail.com>
2024-11-25 09:26:20 +0100

    STATICFILES_DIRS

--- language_learning/settings.py
+++ language_learning/settings.py
@@ -106,9 +106,9 @@ USE_TZ = True
 # Static Files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
