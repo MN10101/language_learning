6eefa2084a106c3a859e41445008be13e3624e56
MN10101 <mamocool3@gmail.com>
2024-11-26 08:46:15 +0100

    Views

--- learning/views.py
+++ learning/views.py
@@ -41,6 +41,7 @@ from django.views.decorators.cache import never_cache
 from django.core.mail import send_mail
 from django.conf import settings
 from django.contrib.auth.models import Group
+from .models import Profile
 
 
 
